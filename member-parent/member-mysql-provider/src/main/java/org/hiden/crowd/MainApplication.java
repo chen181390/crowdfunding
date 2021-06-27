36844aed92c6f7796ade93caecb180d0c93f64d3
陈天瑜 <1813906834@qq.com>
2021-06-27 15:20:32 +0800

    init： 分布式前台

---
+++ member-parent/member-mysql-provider/src/main/java/org/hiden/crowd/MainApplication.java
@@ -0,0 +1,13 @@
+package org.hiden.crowd;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("org.hiden.crowd.mapper")
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ member-parent/readme.md
@@ -0,0 +1,9 @@
+* 端口号：
+  - member-eureka: 1000
+  - member-mysql-provider: 2000
+  - member-redis-provider: 3000
+  - member-authentication-consumer: 4000
+  - member-project-consumer: 5000
+  - member-order-consumer: 7000
+  - member-pay-consumer: 8000
+  - member-zuul: 80
